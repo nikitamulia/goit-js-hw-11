2f0fcc23f2b553177f0ecf614ae07cff8205ef45
nikitamulia <crovone96@gmail.com>
2022-10-26 23:22:38 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startRef = document.querySelector('button[data-start]');
+const stopRef = document.querySelector('button[data-stop]');
 
+startRef.addEventListener('click', changeColor);
+stopRef.addEventListener('click', stopChangeColor);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function changeColor(){ timerId = setInterval(()=> {
+document.body.style.backgroundColor = getRandomHexColor()
+startRef.disabled = true;
+}, 1000);
+}
+
+function stopChangeColor(){
+    clearInterval(timerId);
+    startRef.disabled = false;
+}
