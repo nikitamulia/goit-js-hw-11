37b19cc1f268ecf1d8c0fd9140a2439f53c38cb5
nikitamulia <crovone96@gmail.com>
2022-11-04 17:05:49 +0200

    fix

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -1,23 +1,15 @@
-import axios from 'axios';
-export let page = 1;
-export let query = null;
 
+import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '31043005-c343c849c0779aac8ee4cbc3d';
 const params = `?key=${KEY}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
-
-export async function getPictures(searchQuery) {
-  if (searchQuery !== query) {
-    page = 1;
-    query = searchQuery;
-  }
+export async function getPictures(searchQuery, page) {
   try {
     const response = await axios.get(
-      `${BASE_URL}${params}&q=${query}&page=${page}`
+      `${BASE_URL}${params}&q=${searchQuery}&page=${page}`
     );
-    page += 1;
     return response.data;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,55 +1,49 @@
+
+
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { getPictures, page, query } from './api/fetchApi';
+import { getPictures } from './api/fetchApi';
 import { createMarkup } from './api/galleryCard';
-
-
 const formRef = document.querySelector('#search-form');
 const galleryRef = document.querySelector('.gallery');
 const loadRef = document.querySelector('.js-load-btn');
-
 formRef.addEventListener('submit', onSubmit);
 loadRef.addEventListener('click', onLoadClick);
-
 let lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
-
-
+let page = 1;
+let searchQuery = '';
 async function onSubmit(event) {
   event.preventDefault();
-  const searchQuery = event.currentTarget.elements.searchQuery.value.trim().toLowerCase();
-  
+  searchQuery = event.currentTarget.elements.searchQuery.value
+    .trim()
+    .toLowerCase();
+  page = 1;
+  loadRef.classList.add('js-load-btn');
+  galleryRef.innerHTML = '';
   if (!searchQuery) {
     Notiflix.Notify.failure('Enter a search query!');
     return;
   }
   try {
-    const searchData = await getPictures(searchQuery);
+    const searchData = await getPictures(searchQuery, page);
     const { hits, totalHits } = searchData;
-    
     if (totalHits === 0) {
-      loadRef.classList.add('js-load-btn');
-      galleryRef.innerHTML = '';
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
+    } else if (hits.length > 0) {
+      const markup = hits.map(item => createMarkup(item)).join('');
+      galleryRef.innerHTML = markup;
     }
-      else if(hits.length  > 0){
-        const markup = hits.map(item => createMarkup(item)).join('');
-        galleryRef.innerHTML = markup;
-      }
-      Notiflix.Notify.success(`Hooray! We found ${totalHits} images!`);
-      
-      
-    
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images!`);
     if (totalHits > 40) {
       loadRef.classList.remove('js-load-btn');
-     
     }
     lightbox.refresh();
   } catch (error) {
@@ -57,41 +51,28 @@ async function onSubmit(event) {
     console.log(error);
   }
 }
-
 async function onLoadClick() {
   loadRef.disabled = true;
-  // loadRef.classList.add('js-load-btn');
-  // setTimeout(() => {
-  //   timerLoadBtn();
-  // }, 2000);
-  try{
-    const response = await getPictures(query);
+  loadRef.classList.add('js-load-btn');
+  page += 1;
+  try {
+    const response = await getPictures(searchQuery, page);
     const { hits, totalHits } = response;
-    
     const markup = hits.map(item => createMarkup(item)).join('');
     galleryRef.insertAdjacentHTML('beforeend', markup);
     lightbox.refresh();
-  
-    
-    
-    
-    const amountOfPages = totalHits / 40 - page;
+    const amountOfPages = totalHits / (40 * page);
     loadRef.disabled = false;
+    console.log(amountOfPages);
     if (amountOfPages < 1) {
-     
-      loadRef.classList.add('js-load-btn');
       Notiflix.Notify.info(
         "We're sorry, but you've reached the end of search results."
       );
+    } else {
+      loadRef.classList.remove('js-load-btn');
     }
-  }
-  catch(error){
+  } catch (error) {
     Notiflix.Notify.failure('Something went wrong! Please retry');
     console.log(error);
   }
- 
 }
-
-// function timerLoadBtn(){
-//   loadRef.classList.remove('js-load-btn');
-// }
